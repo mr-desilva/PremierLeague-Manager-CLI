d683cfd715cf24f7abd8f65eb76a58c82563f7a8
Andrew De Silva <47147935+ANDREWDESILVASL@users.noreply.github.com>
2020-11-15 17:42:58 +0530

    POJO Classes

---
+++ Pojo/SchoolFootballClub.java
@@ -0,0 +1,111 @@
+public class SchoolFootballClub {
+    private String clubName;
+    private String clubLocation;
+    private int wins;
+    private int draws;
+    private int defeats;
+    private int goalsReceived;
+    private int goalScored;
+    private int numberOfPoints;
+    private int numberOfPlayedMatches;
+    private String schoolName;
+
+    public SchoolFootballClub() {
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
+
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
+
+    public String getClubLocation() {
+        return clubLocation;
+    }
+
+    public void setClubLocation(String clubLocation) {
+        this.clubLocation = clubLocation;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getDefeats() {
+        return defeats;
+    }
+
+    public void setDefeats(int defeats) {
+        this.defeats = defeats;
+    }
+
+    public int getGoalsReceived() {
+        return goalsReceived;
+    }
+
+    public void setGoalsReceived(int goalsReceived) {
+        this.goalsReceived = goalsReceived;
+    }
+
+    public int getGoalScored() {
+        return goalScored;
+    }
+
+    public void setGoalScored(int goalScored) {
+        this.goalScored = goalScored;
+    }
+
+    public int getNumberOfPoints() {
+        return numberOfPoints;
+    }
+
+    public void setNumberOfPoints(int numberOfPoints) {
+        this.numberOfPoints = numberOfPoints;
+    }
+
+    public int getNumberOfPlayedMatches() {
+        return numberOfPlayedMatches;
+    }
+
+    public void setNumberOfPlayedMatches(int numberOfPlayedMatches) {
+        this.numberOfPlayedMatches = numberOfPlayedMatches;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    @Override
+    public String toString() {
+        return "SchoolFootballClub{" +
+                "clubName='" + clubName + '\'' +
+                ", clubLocation='" + clubLocation + '\'' +
+                ", wins=" + wins +
+                ", draws=" + draws +
+                ", defeats=" + defeats +
+                ", goalsReceived=" + goalsReceived +
+                ", goalScored=" + goalScored +
+                ", numberOfPoints=" + numberOfPoints +
+                ", numberOfPlayedMatches=" + numberOfPlayedMatches +
+                ", schoolName='" + schoolName + '\'' +
+                '}';
+    }
+}
---
+++ Pojo/SportsClub.java
@@ -0,0 +1,31 @@
+public class SportsClub {
+    private String clubName;
+    private String clubLocation;
+
+    public SportsClub() {
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
+
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
+
+    public String getClubLocation() {
+        return clubLocation;
+    }
+
+    public void setClubLocation(String clubLocation) {
+        this.clubLocation = clubLocation;
+    }
+
+    @Override
+    public String toString() {
+        return "SportsClub{" +
+                "clubName='" + clubName + '\'' +
+                ", clubLocation='" + clubLocation + '\'' +
+                '}';
+    }
+}
